e1e902f7fdb75d4144bc58f46d32bcdef60541e5
Amirreza esm <amirreza.77.esmaeili@gmail.com>
2019-07-02 21:06:06 +0430

    ui improvements

--- src/control/Main.java
+++ src/control/Main.java
@@ -5,16 +5,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        double screenX = Screen.getPrimary().getBounds().getMaxX();
+        double screenY = Screen.getPrimary().getBounds().getMaxY();
         Parent root = FXMLLoader.load(getClass().getResource("view/ui.fxml"));
         primaryStage.setTitle("Miniature processor");
         primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("view/images/icon.png")));
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, screenX * 0.9, screenY * 0.9);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
